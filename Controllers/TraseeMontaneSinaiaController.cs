ca6ad36af6e2ec6a3846b4f79d15295245c08050
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-04-09 16:00:14 +0300

    CRUD Transee Montane API

--- Controllers/TraseeMontaneSinaiaController.cs
+++ Controllers/TraseeMontaneSinaiaController.cs
@@ -13,4 +13,54 @@ public class TraseeMontaneController : ControllerBase
     // GET all action
     [HttpGet]
     public ActionResult<List<TraseuMontan>> GetAll() => TraseeMontaneServices.GetAll();
+
+    // GET by Id action
+    [HttpGet("{id}")]
+    public ActionResult<TraseuMontan> Get(int id)
+    {
+        var traseuMontan = TraseeMontaneServices.Get(id);
+
+        if (traseuMontan is null)
+            return NotFound();
+
+        return traseuMontan;
+    }
+
+    // POST action
+    [HttpPost]
+    public IActionResult Add(TraseuMontan traseuMontan)
+    {
+        TraseeMontaneServices.Add(traseuMontan);
+        return CreatedAtAction(nameof(Add), new { id = traseuMontan.Id }, traseuMontan);
+    }
+
+    // PUT action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, TraseuMontan traseuMontan)
+    {
+        if (id != traseuMontan.Id)
+            return BadRequest();
+
+        var existingTraseuMontan = TraseeMontaneServices.Get(id);
+        if (existingTraseuMontan is null)
+            return NotFound();
+
+        TraseeMontaneServices.Update(traseuMontan);
+
+        return NoContent();
+    }
+
+    // DELETE action
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var traseuMontan = TraseeMontaneServices.Get(id);
+
+        if (traseuMontan is null)
+            return NotFound();
+
+        TraseeMontaneServices.Delete(id);
+
+        return NoContent();
+    }
 }
--- Services/TraseeMontaneServices.cs
+++ Services/TraseeMontaneServices.cs
@@ -6,6 +6,7 @@ public static class TraseeMontaneServices
 {
     static List<TraseuMontan> TraseeMontane { get; }
 
+    static int nextId = 3;
     static TraseeMontaneServices()
     {
         TraseeMontane =
@@ -32,4 +33,30 @@ public static class TraseeMontaneServices
     }
 
     public static List<TraseuMontan> GetAll() => TraseeMontane;
+
+    public static TraseuMontan? Get(int id) => TraseeMontane.FirstOrDefault(p => p.Id == id);
+
+    public static void Add(TraseuMontan traseuMontan)
+    {
+        traseuMontan.Id = nextId++;
+        TraseeMontane.Add(traseuMontan);
+    }
+
+    public static void Delete(int id)
+    {
+        var traseuMontan = Get(id);
+        if (traseuMontan is null)
+            return;
+
+        TraseeMontane.Remove(traseuMontan);
+    }
+
+    public static void Update(TraseuMontan traseuMontan)
+    {
+        var index = TraseeMontane.FindIndex(p => p.Id == traseuMontan.Id);
+        if (index == -1)
+            return;
+
+        TraseeMontane[index] = traseuMontan;
+    }
 }
